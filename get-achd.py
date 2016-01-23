bd51202f1dfa9f09175be2cd9a14450df603fb24
Jim Keener <jim@jimkeener.com>
2016-01-23 05:33:13 -0500

    Set UA properly

--- get-achd.py
+++ get-achd.py
@@ -271,8 +271,8 @@ def extract_record(row):
 
 
 headers = {
-    'HTTP_USER_AGENT': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0',
-    'HTTP_ACCEPT': 'text/html,application/xhtml+xml,application/xml; q=0.9,*/*; q=0.8',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',#'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml; q=0.9,*/*; q=0.8',
     'Content-Type': 'application/x-www-form-urlencoded'
 }
 
